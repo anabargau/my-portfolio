0cd4a9d41106bb74550d0e9b3f254c2f4c56efa3
Ana <anabargau@gmail.com>
2022-10-06 14:37:44 +0300

    added mail to contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,6 +5,12 @@ const Contact = forwardRef((props, ref) => {
     <section ref={ref} className="contact">
       <div className="contact-title">Let's get in touch!</div>
       <div className="contact-icon-container">
+        <a href="mailto:anabargau@gmail.com">
+          <img
+            src="https://gist.githubusercontent.com/anabargau/e52505bef58df93e4f728ad3ca3b96d4/raw/08c0885579378cf3cf41a4630fa71043d9bf1c83/gmail.svg"
+            alt="gmail icon"
+          />
+        </a>
         <a href="https://github.com/anabargau" target="_blank" rel="noreferrer">
           <img
             src="https://gist.githubusercontent.com/anabargau/9033048a5f67b10d5097f59aad1f25ba/raw/9bdfd8f35c4422a4cce0cdb5f81cbf85e701ac76/github.svg"
